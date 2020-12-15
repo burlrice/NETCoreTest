e652b2d6bb105e8bdc54e8eb30702c389e0ff1a2
Burl <burl@burlrice.com>
2020-12-15 16:35:14 -0600

    cleanup

--- NextJet/Test.cpp
+++ NextJet/Test.cpp
@@ -6,7 +6,7 @@ using namespace Diagraph;
 
 Test::Test()
 {
-	//QString str = "TODO"; // uncommenting this line results in: An unhandled exception of type 'System.Windows.Markup.XamlParseException' occurred in System.Private.CoreLib.dll
+	QString str = "TODO"; // uncommenting this line results in: BadImageFormatException: Could not load file or assembly 'NextJet, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null'. An attempt was made to load a program with an incorrect format.
 
 	n = -99;
 }
